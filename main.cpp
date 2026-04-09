aef2fdcf74792d939712d7f07f0d8cd76fce5402
AlexBeck <agolfernh2@gmail.com>
2026-04-08 22:05:19 -0500

    evrhlkjwverlkjelkjewr

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cmath>
+#include "particle.h"
+#include "time.h"
+#include <iomanip>
+#include <cstdlib>
+
+using namespace std;
+
+int main() {
+
+
+
+
+
+
+
+
+    return 0;
+}
